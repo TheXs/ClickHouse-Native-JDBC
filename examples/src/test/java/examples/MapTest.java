146e7f0403fbf5ddcfbd70778a839d2cad9f2e25
liliang <649743135@qq.com>
2021-05-25 16:35:27 +0800

    perf: batch insert test

--- examples/src/test/java/examples/MapTest.java
+++ examples/src/test/java/examples/MapTest.java
@@ -7,10 +7,12 @@ import com.github.housepower.jdbc.data.type.complex.DataTypeString;
 import com.github.housepower.jdbc.data.type.complex.DataTypeTuple;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class MapTest {
 
@@ -33,8 +35,9 @@ public class MapTest {
     @Test
     public void testInsert() throws Exception {
         Class.forName("com.github.housepower.jdbc.ClickHouseDriver");
-        final Object[][] data = new Object[1][2];
+        final Object[][] data = new Object[2][2];
         data[0] = new Object[] {"name", "test"};
+        data[1] = new Object[] {"cop", "24"};
         final ClickHouseStruct struct = new ClickHouseStruct("Map", data);
         try (final Connection connection =
                      DriverManager.getConnection("jdbc:clickhouse://192.168.30.12:9000")) {
@@ -44,6 +47,38 @@ public class MapTest {
             prepareStatement.setObject(1, struct);
             final int effectCnt = prepareStatement.executeUpdate();
             System.out.println(effectCnt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testInsertBatch() throws Exception {
+        Class.forName("com.github.housepower.jdbc.ClickHouseDriver");
+        final Random random = new Random();
+
+        final List<Object[][]> dataList = new ArrayList<>();
+        dataList.add(new Object[][] {new Object[]{"name", "test"},
+                new Object[]{"id", String.valueOf(random.nextInt(100))}});
+        dataList.add(new Object[][] {new Object[]{"name", "test"},
+                new Object[]{"id", String.valueOf(random.nextInt(100))}});
+        dataList.add(new Object[][] {new Object[]{"name", "test"},
+                new Object[]{"id", String.valueOf(random.nextInt(100))}});
+        dataList.add(new Object[][] {new Object[]{"name", "test"},
+                new Object[]{"id", String.valueOf(random.nextInt(100))}});
+        dataList.add(new Object[][] {new Object[]{"name", "test"},
+                new Object[]{"id", String.valueOf(random.nextInt(100))}});
+        try (final Connection connection =
+                     DriverManager.getConnection("jdbc:clickhouse://192.168.30.12:9000")) {
+            final PreparedStatement prepareStatement =
+                    connection.prepareStatement(
+                            "insert into map values(?)");
+            for (Object[][] data : dataList) {
+                prepareStatement.setObject(1, new ClickHouseStruct("Map", data));
+                prepareStatement.addBatch();
+            }
+            final int[] effectCntArr = prepareStatement.executeBatch();
+            Arrays.stream(effectCntArr).forEach(System.out::print);
         } catch (Exception e) {
             e.printStackTrace();
         }
